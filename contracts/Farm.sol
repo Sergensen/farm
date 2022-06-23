ac0239dbe9d896cb4e054c36c32068f9c5dc7178
Sergen Sentürk <Sergen.Sentuerk@ext.experienceone.com>
2022-06-23 14:09:34 +0200

    added roles and rights

--- contracts/Farm.sol
+++ contracts/Farm.sol
@@ -1,38 +1,65 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Farm is Ownable, AccessControl {
+contract Farm is AccessControl, ReentrancyGuard {
+    bytes32 public constant FARMER_ROLE = keccak256("FARMER_ROLE");
+    bytes32 public constant FARM_CHIEF_ROLE = keccak256("FARM_CHIEF_ROLE");
+    uint32 constant minimumVotingPeriod = 1 weeks;
+    uint256 proposalCounter;
+
+    struct Proposal {
+        uint256 id;
+        uint256 livePeriod;
+        uint256 votesFor;
+        uint256 votesAgainst;
+        string description;
+        string callData;
+        bool votingPassed;
+        address target;
+        address proposer;
+    }
+
     event Response (bool success, bytes data);
-      constructor (address root) {
-        _setupRole(DEFAULT_ADMIN_ROLE, root);
-    } 
+    event CornReceived(address indexed fromAddress, uint256 amount);
+    event NewProposal(address indexed farmer, uint256 amount);
+    event SeedsSown(
+        address indexed farmer,
+        address indexed target,
+        string callData
+    );
 
-    modifier onlyAdmin() {
-        require(isAdmin(msg.sender), "Restricted to admins.");
+    mapping(uint256 => Proposal) private proposals;
+    mapping(address => uint256[]) private farmerVotes;
+    mapping(address => uint256) private farmers;
+
+    modifier onlyFarmer() {
+        require(hasRole(FARMER_ROLE, msg.sender), "Restricted to farmers.");
+        _;
+    }
+
+    modifier onlyFarmChief() {
+        require(hasRole(FARM_CHIEF_ROLE, msg.sender), "Restricted to farm chiefs.");
         _;
     }
     
-    function execute(address target, bytes calldata callData) public onlyAdmin {
+
+
+    function execute(address target, bytes calldata callData) public onlyFarmChief {
         (bool success, bytes memory data) = target.call(callData);
         require(success);
 
         emit Response(success, data);
     }
 
-    function isAdmin(address account) public virtual view returns (bool) {
-        return hasRole(DEFAULT_ADMIN_ROLE, account);
+    function addFarmChief(address account) public {
+        grantRole(FARM_CHIEF_ROLE, account);
     }
 
-    function addAdmin(address account) public onlyOwner {
-        grantRole(DEFAULT_ADMIN_ROLE, account);
-    }
-
-    function removeAdmin(address account) public onlyOwner {
-        bool exists = isAdmin(account);
-        require(exists, "Provided account is no admin.");
-        renounceRole(DEFAULT_ADMIN_ROLE, account);
+    function removeFarmChief(address account) public {
+        require(hasRole(FARM_CHIEF_ROLE, account), "Provided account is no admin.");
+        renounceRole(FARM_CHIEF_ROLE, account);
     }
 }
