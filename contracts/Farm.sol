d771d4e9e9109282eb818e9cc6ccbfe63348099d
Sergen Sentürk <Sergen.Sentuerk@ext.experienceone.com>
2022-06-23 11:55:21 +0200

    add access controll

--- contracts/Farm.sol
+++ contracts/Farm.sol
@@ -2,42 +2,37 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Farm is Ownable {
-    string[] private admins;
-
+contract Farm is Ownable, AccessControl {
     event Response (bool success, bytes data);
+      constructor (address root) {
+        _setupRole(DEFAULT_ADMIN_ROLE, root);
+    } 
 
-    function execute(address target, bytes calldata callData) public onlyOwner {
+    modifier onlyAdmin() {
+        require(isAdmin(msg.sender), "Restricted to admins.");
+        _;
+    }
+    
+    function execute(address target, bytes calldata callData) public onlyAdmin {
         (bool success, bytes memory data) = target.call(callData);
         require(success);
 
         emit Response(success, data);
     }
 
-    function getAdmins() public view returns (string[] memory) {
-        return admins;
+    function isAdmin(address account) public virtual view returns (bool) {
+        return hasRole(DEFAULT_ADMIN_ROLE, account);
     }
 
-    function addAdmin(string memory admin) public onlyOwner {
-        require(admins.length < 5, "Cannot add more that 5 admins.");
-        admins.push(admin);
+    function addAdmin(address account) public onlyOwner {
+        grantRole(DEFAULT_ADMIN_ROLE, account);
     }
 
-    function removeAdmin(string memory admin) public onlyOwner {
-        uint index = adminIndex(admin);
-        require(index != 10, "Provided account is no admin.");
-
-        admins[index] = admins[admins.length - 1];
-        admins.pop();
-    }
-
-    function adminIndex(string memory account) private view returns(uint) {
-        for (uint i = 0 ; i < admins.length; i++) {
-            if (keccak256(abi.encodePacked(admins[i])) == keccak256(abi.encodePacked(account))) {
-                return i;
-            }
-        }
-        return 10;
+    function removeAdmin(address account) public onlyOwner {
+        bool exists = isAdmin(account);
+        require(exists, "Provided account is no admin.");
+        renounceRole(DEFAULT_ADMIN_ROLE, account);
     }
 }
